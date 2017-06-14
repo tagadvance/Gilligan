3988d9faa820df5c181051fe1ca379fa276755de
Tag <tagspilman@gmail.com>
2017-06-14 04:38:07 -0700

    UnsupportedOperationException

---
+++ src/tagadvance/gilligan/base/UnsupportedOperationException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tagadvance\gilligan\base;
+
+/**
+ * Thrown to indicate that the requested operation is not supported.
+ */
+class UnsupportedOperationException extends \RuntimeException {
+
+}
