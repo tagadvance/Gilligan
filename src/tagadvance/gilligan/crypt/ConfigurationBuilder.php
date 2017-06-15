0f98e3c25ab39024f7c3cfbce3584b6bef9d4c0c
Tag <tagspilman@gmail.com>
2017-06-15 04:44:23 -0700

    improve documentation

--- src/tagadvance/gilligan/crypt/ConfigurationBuilder.php
+++ src/tagadvance/gilligan/crypt/ConfigurationBuilder.php
@@ -9,122 +9,120 @@ namespace tagadvance\gilligan\crypt;
  * @see http://php.net/manual/en/function.openssl-csr-new.php
  */
 class ConfigurationBuilder {
-
+	
 	const CONFIG_DEBIAN = '/etc/ssl/openssl.cnf'; // debian + derivatives
 	const CONFIG_RHEL = '/etc/pki/tls/openssl.cnf'; // RHEL + derivatives
 	const CONFIG_WINDOWS = null; // FIXME: windows
 	
 	private $args;
-
+	
 	static function builder() {
-		return new self();
+		return new self ();
 	}
 	
 	function __construct() {
-		$this->args = [];
+		$this->args = [ ];
 	}
-
+	
 	/**
 	 *
 	 * @param string $filename        	
-	 * @return ConfigurationBuilder
+	 * @return self
 	 */
 	function setConfigurationFile(string $filename): self {
 		$this->args ['config'] = $filename;
 		return $this;
 	}
-
+	
 	/**
-	 * Selects which digest method to use.
+	 * Select which digest method to use.
 	 *
 	 * @param string $algorithm
 	 *        	e.g. 'SHA512'
-	 * @return ConfigurationBuilder
+	 * @return self
 	 */
 	function setDigestAlgorithm(string $algorithm): self {
 		$this->args ['digest_alg'] = $algorithm;
 		return $this;
 	}
-
+	
 	/**
-	 * Selects which extensions should be used when creating an x509
-	 * certificate.
+	 * Select which extensions should be used when creating an x509 certificate.
 	 *
 	 * @param string $extensions
 	 *        	e.g. 'v3_ca'
-	 * @return ConfigurationBuilder
+	 * @return self
 	 */
-	function setX509Extensions(string $extensions): self{
+	function setX509Extensions(string $extensions): self {
 		$this->args ['x509_extensions'] = $extensions;
 		return $this;
 	}
-
+	
 	/**
-	 * Selects which extensions should be used when creating a CSR.
+	 * Select which extensions should be used when creating a CSR.
 	 *
 	 * @param string $extensions
 	 *        	e.g. 'v3_req'
-	 * @return ConfigurationBuilder
+	 * @return self
 	 */
-	function setRequiredExtensions(string $extensions): self{
+	function setRequiredExtensions(string $extensions): self {
 		$this->args ['req_extensions'] = $extensions;
 		return $this;
 	}
-
+	
 	/**
-	 * Specifies how many bits should be used to generate a private key.
+	 * Specify how many bits should be used to generate a private key.
 	 *
-	 * @param integer $bits
+	 * @param int $bits
 	 *        	e.g. 4096
-	 * @return ConfigurationBuilder
+	 * @return self
 	 */
 	function setPrivateKeyBits(int $bits): self {
 		$this->args ['private_key_bits'] = $bits;
 		return $this;
 	}
-
+	
 	/**
-	 * Specifies the type of private key to create.
-	 * This can be one of OPENSSL_KEYTYPE_DSA, OPENSSL_KEYTYPE_DH or
-	 * OPENSSL_KEYTYPE_RSA. The default value is OPENSSL_KEYTYPE_RSA which is
-	 * currently the only supported key type.
+	 * Specify the type of private key to create.
+	 * This can be one of OPENSSL_KEYTYPE_DSA, OPENSSL_KEYTYPE_DH or OPENSSL_KEYTYPE_RSA. The default value is OPENSSL_KEYTYPE_RSA which is currently the only supported key type.
 	 *
-	 * @param integer $type
+	 * @param int $type
 	 *        	e.g. OPENSSL_KEYTYPE_RSA
-	 * @return ConfigurationBuilder
+	 * @return self
 	 */
-	function setPrivateKeyType(int $type = OPENSSL_KEYTYPE_RSA): self{
+	function setPrivateKeyType(int $type = OPENSSL_KEYTYPE_RSA): self {
 		$this->args ['private_key_type'] = $type;
 		return $this;
 	}
-	
-	// TODO: DSL e.g. [...]->encryptKey($key)->withCypher($cypher)
 	
 	/**
 	 * Should an exported key (with passphrase) be encrypted?
 	 *
 	 * @param bool $b        	
-	 * @return ConfigurationBuilder
+	 * @return self
 	 */
 	function encryptKey(bool $b = true): self {
 		$this->args ['encrypt_key'] = $b;
 		return $this;
 	}
-
+	
 	/**
-	 * One of <a href="http://www.php.net/manual/en/openssl.ciphers.php">cipher
-	 * constants</a>.
+	 * One of <a href="http://www.php.net/manual/en/openssl.ciphers.php">cipher constants</a>.
 	 *
 	 * @param integer $cypher        	
-	 * @return ConfigurationBuilder
+	 * @return self
 	 */
 	function withCypher(int $cypher): self {
 		$this->args ['encrypt_key_cipher'] = $cypher;
 		return $this;
 	}
-
+	
+	/**
+	 *
+	 * @return array
+	 */
 	function build(): array {
 		return $this->args;
 	}
-
+	
 }
