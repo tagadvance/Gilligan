143f34e5653dd6075ea7df743da66d8fcf5bb3a3
Tag <tagspilman@gmail.com>
2017-06-15 03:26:50 -0700

    System

---
+++ src/tagadvance/gilligan/base/System.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace tagadvance\gilligan\base;
+
+use tagadvance\gilligan\util\SystemTimeProvider;
+
+class System {
+	
+	/**
+	 *
+	 * @return integer Returns the current time in milliseconds.
+	 */
+	static function currentTimeMillis() {
+		$provider = new SystemTimeProvider ();
+		return $provider->currentTimeMillis ();
+	}
+
+	/**
+	 *
+	 * @return string
+	 * @see http://php.net/sys_get_temp_dir
+	 */
+	static function getTemporaryDirectory() {
+		return sys_get_temp_dir ();
+	}
+
+	/**
+	 *
+	 * @return array
+	 * @see http://www.php.net/manual/en/function.sys-getloadavg.php
+	 */
+	static function getLoadAverage() {
+		return sys_getloadavg ();
+	}
+
+	static function isCLI() {
+		$sapi = php_sapi_name ();
+		return ($sapi === 'cli'); // TODO: discourage string literals
+	}
+
+	static function isCGI() {
+		$sapi = php_sapi_name ();
+		return String::valueOf ( $sapi )->startsWith ( 'cgi' );
+	}
+	
+	private function __construct() {
+		
+	}
+
+}
