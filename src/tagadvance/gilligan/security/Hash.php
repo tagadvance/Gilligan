4b45df3a4ad9f046a6d070c2c9a4f9c4e6b2a8ba
Tag <tagspilman@gmail.com>
2017-08-20 00:06:49 -0700

    source format

--- src/tagadvance/gilligan/security/Hash.php
+++ src/tagadvance/gilligan/security/Hash.php
@@ -4,31 +4,30 @@ namespace tagadvance\gilligan\security;
 
 class Hash {
 
-	const ALGORITHM_MD5 = 'md5';
+    const ALGORITHM_MD5 = 'md5';
 
-	const ALGORITHM_SHA1 = 'sha1';
+    const ALGORITHM_SHA1 = 'sha1';
 
-	const ALGORITHM_SHA256 = 'sha256';
+    const ALGORITHM_SHA256 = 'sha256';
 
-	const ALGORITHM_SHA512 = 'sha512';
+    const ALGORITHM_SHA512 = 'sha512';
 
-	private function __construct() {
-	}
+    private function __construct() {}
 
-	static function md5($value) {
-		return hash ( self::ALGORITHM_MD5, $value );
-	}
+    static function md5($value) {
+        return hash(self::ALGORITHM_MD5, $value);
+    }
 
-	static function sha1($value) {
-		return hash ( self::ALGORITHM_SHA1, $value );
-	}
+    static function sha1($value) {
+        return hash(self::ALGORITHM_SHA1, $value);
+    }
 
-	static function sha256($value) {
-		return hash ( self::ALGORITHM_SHA256, $value );
-	}
+    static function sha256($value) {
+        return hash(self::ALGORITHM_SHA256, $value);
+    }
 
-	static function sha512($value) {
-		return hash ( self::ALGORITHM_SHA512, $value );
-	}
+    static function sha512($value) {
+        return hash(self::ALGORITHM_SHA512, $value);
+    }
 
 }
