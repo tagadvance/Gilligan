67a7b345cf3909bb1b2662749c7d67d58c926570
Tag <tagspilman@gmail.com>
2017-08-22 04:22:57 -0700

    templates

---
+++ templates/README.md
@@ -0,0 +1,3 @@
+# Templates
+
+These are intended to be copied and pasted as necessary.
---
+++ templates/__Class.php
@@ -0,0 +1,35 @@
+<?php
+
+class __Class {
+
+    function __construct() {}
+
+    function __clone() {}
+
+    function __call($name, $arguments) {}
+
+    function __callstatic($name, $arguments) {}
+
+    function __get($name) {}
+
+    function __set($name, $value) {}
+
+    function __isset($name) {}
+
+    function __unset($name) {}
+
+    function __sleep() {}
+
+    function __wakeup() {}
+
+    function __invoke() {}
+
+    function __toString(): string {}
+
+    function __set_state($array) {}
+
+    function __destruct() {}
+
+    function __debugInfo() {}
+
+}
