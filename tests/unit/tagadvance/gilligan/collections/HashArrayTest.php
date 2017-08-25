dfb8f29b4016dd8441108d8c6155f0dd4f49abd9
Tag <tagspilman@gmail.com>
2017-08-25 15:36:57 -0700

    remove unnecessary unit tests

--- tests/unit/tagadvance/gilligan/collections/HashArrayTest.php
+++ tests/unit/tagadvance/gilligan/collections/HashArrayTest.php
@@ -3,7 +3,6 @@
 namespace tagadvance\gilligan\collections;
 
 use PHPUnit\Framework\TestCase;
-use tagadvance\gilligan\text\StringClass;
 
 class HashArrayTest extends TestCase {
 
@@ -32,33 +31,6 @@ class HashArrayTest extends TestCase {
         // PHPUnit\Framework\Exception: Argument #1 (No Value) of PHPUnit\Framework\Assert::assertArrayHasKey() must be a integer or string
         // $this->assertArrayHasKey($key, $array);
         $this->assertEquals($expected, $array[$key]);
-    }
-
-    function testObjectIsSetAndUnset() {
-        $array = new HashArray();
-        // $key object must implement __toString
-        $key = new StringClass('foo');
-        
-        $this->assertFalse(isset($array->$key));
-        
-        $array->$key = true;
-        
-        $this->assertTrue(isset($array->$key));
-        
-        unset($array->$key);
-        
-        $this->assertFalse(isset($array->$key));
-    }
-
-    function testObjectSetAndGetWithObjectImplementsToString() {
-        $expected = 'value';
-        
-        $array = new HashArray();
-        // $key object must implement __toString
-        $key = new StringClass('foo');
-        $array->$key = $expected;
-        
-        $this->assertEquals($expected, $array->$key);
     }
 
 }
