ac60eeaaa27669645ed97ac8f99601c68d00bbb3
Tag <tagspilman@gmail.com>
2017-08-26 17:19:48 -0700

    replace getRealPath() with getPathname()

--- src/tagadvance/gilligan/io/FileOutputStream.php
+++ src/tagadvance/gilligan/io/FileOutputStream.php
@@ -5,7 +5,7 @@ namespace tagadvance\gilligan\io;
 class FileOutputStream extends ResourceOutputStream {
 
 	function __construct(File $file, $append = false) {
-		$path = $file->getRealPath();
+	    $path = $file->getPathname();
 		$mode = $append ? FileMode::WRITE_ONLY_APPEND : FileMode::WRITE_ONLY_TRUNCATE;
 		$handle = fopen ( $path, $mode );
 		parent::__construct ( $handle );
